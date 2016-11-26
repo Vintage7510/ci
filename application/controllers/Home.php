e1d93d924e76c11541468e03c3774de6fe06a6ef
sattawat jaiyen <sattawat_jaiyen@outlook.com>
2016-11-26 14:48:56 +0700

    เพิ่ม htaccess

---
+++ application/controllers/Home.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: lab
+ * Date: 26/11/2559
+ * Time: 13:48
+ */
+class Home extends CI_Controller
+{
+    public function index()
+    {
+        print "Hello";
+    }
+    
+}
