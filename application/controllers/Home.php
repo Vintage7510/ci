d5a6105f78adb2f3943f32244d880bd8de7d9be7
sattawat jaiyen <sattawat_jaiyen@outlook.com>
2016-11-26 16:26:20 +0700

    เพิ่ม footer ในงาน

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -10,7 +10,15 @@ class Home extends CI_Controller
 {
     public function index()
     {
+        $this->load->view('layout/header_view');
         $this->load->view('home_view');
+    }
+
+    public function test()
+    {
+        $this->load->view('layout/header_view');
+        $this->load->view('test_view');
+        $this->load->view('layout/footer_view');
     }
 
 }
