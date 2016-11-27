bc520b8d46d5815d8de2c9ecea5107445dccbdbf
sattawat jaiyen <sattawat_jaiyen@outlook.com>
2016-11-27 14:37:49 +0700

    การจัดหน้า category_view

---
+++ application/views/admin/category_view.php
@@ -0,0 +1,33 @@
+admin_view.php<!-- Page Content -->
+<div id="page-wrapper">
+    <div class="row">
+        <div class="col-lg-12">
+            <h1 class="page-header">จัดการหมวดหมู่</h1>
+        </div>
+        <!-- /.col-lg-12 -->
+
+        <table class="table">
+            <div class="col-lg-12">
+                <td>
+                    <a href="#" class="btn btn-sm btn-primary"><i class="fa fa-plus"></i> เพิ่มหมวดหมู่</a>
+                </td>
+                <tr>
+                    <th>#</th>
+                    <th>ชื่อหมวดหมู่</th>
+                    <th>จัดการ</th>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>กีฬา</td>
+                    <td>
+                        <a href="#" class="btn btn-sm btn-warning"><i class="fa fa-edit"></i> แก้ไข</a>
+                        <a href="#" class="btn btn-sm btn-danger"><i class="fa fa-recycle"></i> ลบ</a>
+                    </td>
+
+                </tr>
+            </div>
+        </table>
+    </div>
+    <!-- /.row -->
+</div>
+<!-- /#page-wrapper -->
